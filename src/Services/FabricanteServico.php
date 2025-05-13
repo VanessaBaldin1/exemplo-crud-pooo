c3e0cdd46b96330cd23ae5c6da20c446420ed5dc
Vanessa Baldin <vanbaldin08@gmail.com>
2025-05-13 08:46:01 -0300

    Adiciona metodo buscarPorId

--- src/Services/FabricanteServico.php
+++ src/Services/FabricanteServico.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace ExemploCrud\Services;
 
 use Exception;
@@ -8,8 +9,9 @@ use Throwable;
 use ExemploCrud\Database\ConexaoBD; //precisa se atentar com a pasta que esta o ConexaoBD
 use ExemploCrud\Models\Fabricante;
 
-final class FabricanteServico{
-  
+final class FabricanteServico
+{
+
   private PDO $conexao;
 
   public function __construct()
@@ -17,27 +19,27 @@ final class FabricanteServico{
     $this->conexao = ConexaoBD::getConexao(); //acessando um recurso da classe não é herança
   }
 
-   public function listarTodos():array {
+  public function listarTodos(): array
+  {
     $sql = "SELECT * FROM fabricantes ORDER BY nome";
-    
+
     try {
       /* Preparando o comando SQL ANTES de executar no servidor e guardando em memória (variável consulta ou query) */
       $consulta = $this->conexao->prepare($sql);
-  
+
       /* Executando o comando no banco de dados*/
-  
+
       $consulta->execute();
-  
+
       /* "fetch All", Busca/Retorna todos os dados provenientes da execução da consulta e os transforma em um array associativo */
       return $consulta->fetchAll(PDO::FETCH_ASSOC);
-  
     } catch (Throwable $erro) {
-       throw new Exception("Erro ao carregar fabricantes: ".$erro-> getMessage());
+      throw new Exception("Erro ao carregar fabricantes: " . $erro->getMessage());
     }
   }
-  
-   public function inserir(Fabricante $fabricante): void
-   {
+
+  public function inserir(Fabricante $fabricante): void
+  {
     $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
 
     try {
@@ -45,23 +47,29 @@ final class FabricanteServico{
       $consulta->bindValue(":nome", $fabricante->getNome(), PDO::PARAM_STR);
       $consulta->execute();
     } catch (Throwable $erro) {
-      throw new Exception("Erro ao inserir: " .$erro->getMessage());
+      throw new Exception("Erro ao inserir: " . $erro->getMessage());
+    }
+  }
+
+  public function buscarPorId(int $id): ?array
+  {
+    $sql = "SELECT * FROM fabricantes WHERE id= :id";
+
+    try {
+      $consulta = $this->conexao->prepare($sql);
+      $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+      $consulta->execute();
+      //Usar somente Fetch para chamar um só linha de registro, não todos como estava antes fetchAll (chama todos os registros constam na tabela)
+      //Usamos o fetch para garantir o retorno de um único array associativo com o resultado
+      return $consulta->fetch(PDO::FETCH_ASSOC);
+    } catch (Throwable $erro) {
+      throw new Exception("Erro ao carregar fabricante: " . $erro->getMessage());
     }
     
-
-
-
-
-
-
-
-   }
-
-
-
-
-
-
   }
 
 
+
+
+
+}
